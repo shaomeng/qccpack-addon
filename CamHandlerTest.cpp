9be04420c7d437d3d98d64c315169c32180d806f
Samuel Li <shaomenglee@gmail.com>
2016-01-11 11:04:40 -0800

    CamHandlerTest reads from NetCDF now

--- CamHandlerTest.cpp
+++ CamHandlerTest.cpp
@@ -46,15 +46,114 @@ int main( int argc, char* argv[] )
     }
     float targetRate = atof( argv[1] );
 
-    char asciiInput[] = "./HommeMap/FSDSC";
-    char filename[] = "./dump/bit.stream";
     string hommeMap   = "./HommeMap/reorderedCbasedHommeMapping.nc";
     string faceMap    = "./HommeMap/faceIdsNeNp304.nc";
+	string netcdfInput = "/opt/Research_Data/ensemble_orig/cesm1_1.FC5.ne30_g16.000.cam.h0.0001-01-01-00000.nc";
+    string varname    = "LCLOUD";
 
-    size_t LEV = 1;
+    /* Examine if the file and variable valid */
+    VAPoR::NetCDFSimple ncsimple;
+    int rc = ncsimple.Initialize( netcdfInput );
+    if( rc < 0 )
+    {
+        cerr << "ncsimple.Initialize() error: " << netcdfInput << endl;
+        exit(1);
+    }
+
+    vector <VAPoR::NetCDFSimple::Variable> vars = ncsimple.GetVariables();
+    int varIdx = -1;
+    for( int i = 0; i < vars.size(); i++ )
+    {
+        if( vars[i].GetName().compare( varname ) == 0 )
+        {
+            cout << "Found variable: " << vars[i].GetName() << endl;
+            varIdx = i;
+            break;
+        }
+    }
+    assert( varIdx != -1 );
+    vector< string > varDimNames = vars[varIdx].GetDimNames();
+    for( int j = 0; j < varDimNames.size(); j++ )
+        cout << "\tdimension name: " << varDimNames[j]
+             << ",\tlength: " << ncsimple.DimLen(varDimNames[j]) << endl;
+
+    /* make sure this is a normal 3D variable by testing it's dimension names */
+    /* 2D variables are not supported here */
+    assert( varDimNames.size() == 3 );
+    assert( varDimNames[0].compare( "time" ) == 0 );
+    assert( varDimNames[1].compare( "lev" ) == 0 );
+    assert( varDimNames[2].compare( "ncol" ) == 0 );
+
+    size_t lev = ncsimple.DimLen( "lev" );
+    size_t ncol = ncsimple.DimLen( "ncol" );
+    size_t start[] = {1, 0, 0};
+    size_t count[] = {1, lev, ncol};
+
+    size_t homme_size = lev * ncol;
+    float* homme_buf = new float[ homme_size ];
+
+    ncsimple.OpenRead( vars[varIdx] );
+    rc = ncsimple.Read( start, count, homme_buf );
+    ncsimple.Close();
+    if( rc < 0 )
+    {
+        cerr << "ncsimple.Read() error: " << endl;
+        exit(1);
+    }
+
+    /* Evaluation:
+     * This chunk of code uses SPECK to encode, then decode, and evaluate.
+     */
+	VAPoR::CamHandler handler( hommeMap, faceMap );
     int numXYDWTLevels = 4;
     int numZDWTLevels = 2;
+    char filename[] = "./dump/bit.stream";
+    float* homme_reconstruct = new float[ homme_size ];
+    handler.speckEncode2Dp1D ( homme_buf, homme_size, lev, numXYDWTLevels, 
+                               numZDWTLevels, targetRate, filename );
+    handler.speckDecode3D( filename, homme_size, lev, homme_reconstruct );
 
+    double rmse, lmax, nrmse, nlmax, minA, maxA, minB, maxB, meanA, meanB;
+    handler.evaluate2arrays( homme_buf, homme_reconstruct, homme_size,
+                             &rmse, &lmax,
+                             &nrmse, &nlmax,
+                             &minA, &maxA,
+                             &minB, &maxB,
+                             &meanA, &meanB );
+    printf("Groud truth: mean = %.8e, min = %e, max = %e\n", meanA, minA, maxA );
+    printf("Reconstruct: mean = %.8e, min = %e, max = %e\n", meanB, minB, maxB );
+    printf("Mean Difference: %e\n", meanA - meanB );
+    printf( "Reconstruction RMS  = %e, LMAX  = %e\n", rmse, lmax );
+    printf( "Reconstruction NRMS = %e, NLMAX = %e\n", nrmse, nlmax );
+
+/*
+	size_t pos_count = 0;
+    size_t neg_count = 0;
+    for( size_t i = 0; i < homme_size; i++ )
+    {
+        if( homme_buf[i] - homme_reconstruct[i] > 0 )
+        {
+            pos_count++;
+            printf("%e ,\t%e,\t+ \n", homme_buf[i], homme_reconstruct[i]);
+        }
+        else if( homme_buf[i] - homme_reconstruct[i] < 0 )
+        {
+            neg_count++;
+            printf("%e ,\t%e,\t- \n", homme_buf[i], homme_reconstruct[i]);
+        }
+        else
+            printf("%e ,\t%e,\t= \n", homme_buf[i], homme_reconstruct[i]);
+    }
+    printf("plus = %lu, minus = %lu\n", pos_count, neg_count );
+*/
+
+    delete[] homme_reconstruct;
+    /* End Evaluation */
+
+
+	delete[] homme_buf;
+
+/*
     size_t homme_size = _NCOL * LEV;
     float* homme_buf = new float[ homme_size ];
     cerr << "start reading input... " << endl;
@@ -65,13 +164,10 @@ int main( int argc, char* argv[] )
 
     float* homme_buf_comp = new float[ homme_size ];
 
-/*
     cerr << "start 3D DWT and SPECK encoding... " << endl;
     handler.speckEncode3D( homme_buf, homme_size, LEV,
                            numZDWTLevels, targetRate, filename );
-*/
 
-/*
     cerr << "start 2D+1D DWT and SPECK encoding... " << endl;
     handler.speckEncode2Dp1D( homme_buf, homme_size, LEV,
                            numXYDWTLevels, numZDWTLevels, targetRate, filename );
@@ -79,8 +175,6 @@ int main( int argc, char* argv[] )
 
     cerr << "start 3D decoding... " << endl;
     handler.speckDecode3D( filename, homme_size, LEV, homme_buf_comp ); 
-*/
-
 
     cerr << "start 2D SPECK encoding... " << endl;
     handler.speckEncode2D( homme_buf, homme_size, 
@@ -88,36 +182,8 @@ int main( int argc, char* argv[] )
     cerr << "start 2D decoding... " << endl;
     handler.speckDecode2D( filename, homme_size, homme_buf_comp );
 
-
-    
-    double rmse, lmax, nrmse, nlmax, minA, maxA, minB, maxB, meanA, meanB;
-    handler.evaluate2arrays( homme_buf, homme_buf_comp, homme_size, 
-                             &rmse, &lmax, 
-                             &nrmse, &nlmax, 
-                             &minA, &maxA,
-                             &minB, &maxB,
-                             &meanA, &meanB );
-
-    cerr << "1st array min = " << minA << ", max = " << maxA << ", mean = " << meanA << endl;
-    cerr << "2nd array min = " << minB << ", max = " << maxB << ", mean = " << meanB << endl;
-    cerr << "RMS  = " << rmse << ",  LMax  = " << lmax << endl;
-    cerr << "NRMS = " << nrmse << ", NLMax = " << nlmax << endl;
-
-    double Ai, Bi;
-    double max = 0.0;
-    int idx = 0;
-    for( int i = 0; i < homme_size; i++ ) {
-        double tmp = fabs( homme_buf[i] - homme_buf_comp[i] );
-        if( tmp > max ) {
-            idx = i;
-            max = tmp;
-            Ai = homme_buf[i];
-            Bi = homme_buf_comp[i];
-        }
-    }
-    printf("\nAt index %d, (%e) - (%e) = (%e)\n", idx, Ai, Bi, max );
-        
-
     delete[] homme_buf;
     delete[] homme_buf_comp;
+*/
+
 }
